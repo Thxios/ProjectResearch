d8286bbaa9e03a21810e3987f0d6ea3c91bbb536
Thxios <ljm0302111@naver.com>
2020-04-26 23:15:11 +0900

    Main Game Manager

--- Game/Game.py
+++ Game/Game.py
@@ -28,20 +28,20 @@ class Game:
         return self.background
 
     def draw_object(self, obj: Object):
-        x_start = max(obj.x, 0)
-        y_start = max(obj.y, 0)
-        x_end = min(obj.x + obj.w, SCREEN_WIDTH)
-        y_end = min(obj.y + obj.h, SCREEN_HEIGHT)
+        x_start = max(obj.x_int, 0)
+        y_start = max(obj.y_int, 0)
+        x_end = min(obj.x_int + obj.w, SCREEN_WIDTH)
+        y_end = min(obj.y_int + obj.h, SCREEN_HEIGHT)
 
         if x_end < 0 or y_end < 0:
             return
         if x_start >= SCREEN_WIDTH or y_start >= SCREEN_HEIGHT:
             return
 
-        obj_x_start = max(-obj.x, 0)
-        obj_y_start = max(-obj.y, 0)
-        obj_x_end = min(SCREEN_WIDTH - obj.x, obj.w)
-        obj_y_end = min(SCREEN_HEIGHT - obj.y, obj.h)
+        obj_x_start = max(-obj.x_int, 0)
+        obj_y_start = max(-obj.y_int, 0)
+        obj_x_end = min(SCREEN_WIDTH - obj.x_int, obj.w)
+        obj_y_end = min(SCREEN_HEIGHT - obj.y_int, obj.h)
 
         # print((obj_x_start, obj_x_end), (obj_y_start, obj_y_end))
         # print(self.background[x_start: x_end, y_start: y_end].shape)
